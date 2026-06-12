72ebcd6c66e67a44af94d2222d24e0a9a8ebbfc0
huangshuheng <3444104242@qq.com>
2026-06-12 13:01:58 +0800

    fix: db

--- config/db.js
+++ config/db.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose'
 // 连接数据库
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL)
+    await mongoose.connect(
+      'mongodb://mongo:KJEwshffbKgjHSOPVCECidoQCJCBUVNQ@thomas.proxy.rlwy.net:48720'
+    )
     console.log('连接成功')
   } catch (error) {
     console.log('连接失败', error)
